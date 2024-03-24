8ac80d38c90f95cc814167c2ae0ee6980a534265
Vanina <vaniibalbastro@gmail.com>
2024-03-24 00:20:33 -0300

    cambio de index por indexpaciente y Primer Pagina por index

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-
-const dotContainer = document.querySelector('.slider-dots');
-
-
-for (let i = 0; i < totalSlides; i++) {
-    const dot = document.createElement('span');
-    dot.classList.add('slider-dot');
-    if (i === 0) {
-        dot.classList.add('active'); 
-    }
-    dot.addEventListener('click', () => {
-        goToSlide(i);
-    });
-    dotContainer.appendChild(dot);
+function redirectTo(page) {
+    window.location.href = page;
 }
