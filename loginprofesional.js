d2b22ef7fc3ce1aa41a948354aa15edae61b34d9
Vanina <vaniibalbastro@gmail.com>
2024-03-26 19:46:35 -0300

    otraactualizacion

--- loginprofesional.js
+++ loginprofesional.js
@@ -1,49 +1,44 @@
-
 function validateForm(event) {
-    
     event.preventDefault();
 
-  
     const email = document.getElementById("email").value;
     const contraseña = document.getElementById("contraseña").value;
 
-  
     if (email === "" || contraseña === "") {
         alert("Por favor complete todos los campos.");
         return false;
     }
 
-   
     const registros = JSON.parse(localStorage.getItem("usuarios"));
 
-    
     let usuarioEncontrado = false;
+    let contraseñaCorrecta = false;
+
     if (registros) {
         registros.forEach(function(registro) {
             if (registro.email === email) {
                 if (registro.contraseña === contraseña) {
                     usuarioEncontrado = true;
-                    return;
+                    contraseñaCorrecta = true;
+                    return; // No es necesario seguir iterando si se encontró una coincidencia
                 } else {
-                    alert("Contraseña incorrecta.");
-                    return false; 
+                    contraseñaCorrecta = false;
+                    return; // No es necesario seguir iterando si se encontró una coincidencia con email pero contraseña incorrecta
                 }
             }
         });
     }
 
-
-    if (usuarioEncontrado) {
+    if (usuarioEncontrado && contraseñaCorrecta) {
         window.location.href = "perfilmedico.html";
+    } else if (usuarioEncontrado && !contraseñaCorrecta) {
+        alert("Contraseña incorrecta.");
     } else {
         alert("Usuario no registrado.");
-
         window.location.href = "regmed.html";
     }
 
-
     return false;
 }
-
 
 document.getElementById("loginForm").addEventListener("submit", validateForm);
